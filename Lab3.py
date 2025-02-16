b7ee94510f2beed24ddfecd1143e277a3e784640
roseli610 <rlbui2007@gmail.com>
2025-02-16 15:31:30 -0500

    Update Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -1,8 +1,12 @@
 # Function 1 (30): Convert the given polar coordinates (r,θ) to Cartesian coordinates (x,y). 
 # This function should take the polar coordinates (r,θ) and return Cartesian coordinates (x,y), where θ=theta.
 def polar_to_cartesian(r,θ):
-    # TODO: Implement this function
-    pass  # Replace with your code
+if 0 | 2 * math.pi:
+    0 = math.radians(0)
+    x = r * math.cos(0)
+    y = r * math.sin(0)
+
+    return round(x, 5), round(y, 5)
 
 # Function 2(30): Convert Cartesian coordinates (x,y) to polar coordinates (r,θ) , where θ=theta.
 # This function should take the Cartesian coordinates (x,y) as input and return the polar coordinates (r,θ).
